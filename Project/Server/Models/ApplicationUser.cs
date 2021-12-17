006e74032fe80edcebcef519832947c24a604368
vincent <vincent@LAPTOP>
2021-12-17 18:50:29 +0800

    Add project files.

---
+++ Project/Server/Models/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project.Server.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public String FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
